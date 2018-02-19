a4dbaf2626406351accf52c8ae3e336aaa3db1da
Srinivasulu3264 <srinivasulu.budharapu@vmokshagroup.com>
2018-02-19 15:56:30 +0530

    added action for menuButtons in homeviewcontroller

--- BeerboardBeerchip/ContainerViewController.swift
+++ BeerboardBeerchip/ContainerViewController.swift
@@ -30,6 +30,30 @@ class ContainerViewController: UIViewController  {
    
     @IBAction func locationButtonAction(_ sender: Any) {
         
+        let  locatorVC  = self.storyboard?.instantiateViewController(withIdentifier: "LocatorViewController") as! LocatorViewController
+        locatorVC.transitioningDelegate = self
+        self.present(locatorVC , animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func orderButtonAction(_ sender: Any) {
+        
+        let  orderVC  = self.storyboard?.instantiateViewController(withIdentifier: "OrderViewController") as! OrderViewController
+        orderVC.transitioningDelegate = self
+        self.present(orderVC , animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func rewardsButtonAction(_ sender: Any) {
+        
+        let  rewardsVC  = self.storyboard?.instantiateViewController(withIdentifier: "RewardsViewController") as! RewardsViewController
+        rewardsVC.transitioningDelegate = self
+        self.present(rewardsVC , animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func moreButtonAction(_ sender: Any) {
+        
         let  moreVC  = self.storyboard?.instantiateViewController(withIdentifier: "MoreViewController") as! MoreViewController
         moreVC.transitioningDelegate = self
         self.present(moreVC , animated: true, completion: nil)
